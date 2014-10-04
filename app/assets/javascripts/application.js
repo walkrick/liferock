f36875652c0d703aa23ab8766b781543f392e72c
Savannah Worth <sdworth11@gmail.com>
2014-10-03 18:01:51 -0600

    changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,39 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+  console.log('here');
+
+
+  $('#dance').click(function () {
+    console.log('here');
+
+    var change = 100;
+    $('#rock').css('top', 300 - change + 'px');
+    $('#rock').css('left', 250 - change + 'px');
+
+    for (var i = 0; i < 3; i++) {
+
+      var time = 5000 * i;
+
+      window.setTimeout(function () {
+        change = Math.floor((Math.random() * 100 ) + 10);
+
+        console.log(change);
+
+        $('#rock').css('top', 300 - change + 'px');
+        $('#rock').css('left', 250 + change + 'px');
+
+      }, time);
+
+    }
+
+    console.log('here');
+
+    window.setTimeout(function () {
+
+      console.log('here too');
+      $('#rock').css('top', 300 + 'px');
+      $('#rock').css('left', 250 + 'px')
+    }, 20000);
+  })
